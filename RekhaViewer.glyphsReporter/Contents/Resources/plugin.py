7a54992efc5b85ec533734327c5c917c4a8a4046
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-03-06 08:19:17 +0100

    Preparing display of caps in Viewer
    Doesn’t work yet

--- RekhaViewer.glyphsReporter/Contents/Resources/plugin.py
+++ RekhaViewer.glyphsReporter/Contents/Resources/plugin.py
@@ -11,13 +11,53 @@
 #
 ###########################################################################################################
 
-
+from GlyphsApp import *
 from GlyphsApp.plugins import *
 
 class RekhaViewer(ReporterPlugin):
 
 	def settings(self):
 		self.menuName = u'Rekha'
+	
+	def rekhaBezierForMasterID(self, rekha, masterID, capName=""):
+		layer = GSLayer()
+		layer.setAssociatedMasterId_(masterID)
+		origin = rekha.origin
+		width = rekha.size.width
+		height = rekha.size.height
+		
+		# create rectangle path:
+		rectangle = GSPath()
+		nodePositions = (
+			NSPoint( origin.x,       origin.y ),
+			NSPoint( origin.x+width, origin.y ),
+			NSPoint( origin.x+width, origin.y+height ),
+			NSPoint( origin.x,       origin.y+height ),
+		)
+		for thisPosition in nodePositions:
+			newNode = GSNode()
+			newNode.position = thisPosition
+			newNode.type = LINE
+			rectangle.nodes.append(newNode)
+		rectangle.closed = True
+		
+		# correct path direction:
+		if rectangle.direction != -1:
+			rectangle.reverse()
+			
+		# insert rectangle into layer:
+		layer.paths.append(rectangle)
+		
+		for i in (-1,1):
+			cap = GSHint()
+			cap.type = CAP
+			cap.name = capName
+			cap.originNode = rectangle.nodes[i]
+			cap.setOptions_(3) # fit
+			layer.addHint_(cap)
+		
+		# return the NSBezierPath
+		return layer.bezierPath
 	
 	def drawRekha(self, layer):
 		defaults = (700,100,20) # height, thickness, overshoot
@@ -47,21 +87,30 @@ class RekhaViewer(ReporterPlugin):
 					RekhaOvershoot = defaults[2]
 		
 				xOrigin = -RekhaOvershoot
-
+				
+				# add caps if present in font:
+				capName = None
+				font = layer.parent.parent
+				if font:
+					if font.glyphs["_cap.rekha"]:
+						capName = "_cap.rekha"
+				
 				if layer.anchors["rekha_stop"]:
 					stopPosition = layer.anchors["rekha_stop"].position.x
 					LeftRekha = NSRect()
 					LeftRekha.origin = NSPoint(xOrigin, RekhaHeight)
 					LeftRekha.size = NSSize(stopPosition-xOrigin, RekhaThickness)
-					NSBezierPath.fillRect_(LeftRekha)
-
+					LeftRekhaBezierPath = self.rekhaBezierForMasterID(LeftRekha, layer.associatedMasterId, capName)
+					LeftRekhaBezierPath.fill()
+					
 				if layer.anchors["rekha"]:
 					xOrigin = layer.anchors["rekha"].position.x
 		
 				Rekha = NSRect()
 				Rekha.origin = NSPoint(xOrigin, RekhaHeight)
 				Rekha.size = NSSize(layer.width+RekhaOvershoot-xOrigin, RekhaThickness)
-				NSBezierPath.fillRect_(Rekha)
+				RekhaBezierPath = self.rekhaBezierForMasterID(Rekha, layer.associatedMasterId, capName)
+				RekhaBezierPath.fill()
 	
 	def background(self, layer):
 		NSColor.grayColor().set()
